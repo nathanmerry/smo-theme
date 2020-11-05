1e68ec74ee9ab222d31d1a772bf3133535c7c2bf
Nathan <nathanmerry9713@gmail.com>
2020-11-05 19:44:22 +0000

    fix website id class

--- website-id.php
+++ website-id.php
@@ -6,11 +6,12 @@ class WebsiteID
 
 	function __construct()
 	{
-		$websites = json_decode(file_get_contents('../websites.json'), true);
+		$websites = json_decode(file_get_contents(__DIR__ . '/websites.json'), true);
 		$key = array_search($_SERVER['HTTP_HOST'], array_column($websites, 'name'));
 		$this->id = $websites[$key]['id'];
 	}
 }
+
 
 $websiteId = new WebsiteID();
 $websiteId = $websiteId->id;
